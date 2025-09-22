50d05bd437f8ac9f9e79a0da3c77a0b3920652ee
Hari <hariprasadlucky20@gmail.com>
2025-09-22 21:32:30 +0530

    Update WebConfig.java

--- src/main/java/com/example/Library_Management/WebConfig.java
+++ src/main/java/com/example/Library_Management/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // your React app
+                        .allowedOrigins("https://libraryapp-3xxi.onrender.com") // your React app
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
